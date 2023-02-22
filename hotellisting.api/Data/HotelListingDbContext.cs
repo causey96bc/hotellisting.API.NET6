1f7c119fca5fd02d30f99929bf8507ce687ea080
rfbrett <brett.causey@rfsmart.com>
2023-02-22 14:18:02 -0500

    adding dbInitialization on program

---
+++ hotellisting.api/Data/HotelListingDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace hotellisting.api.Data
+{
+    public class HotelListingDbContext: DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options): base(options)
+        {
+          
+        }
+    }
+}
--- hotellisting.api/Program.cs
+++ hotellisting.api/Program.cs
@@ -1,13 +1,17 @@
+using hotellisting.api.Data;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("HotelListingDbConnectionString");
 
 // Add services to the container.
-
+builder.Services.AddDbContext<HotelListingDbContext>(options => { options.UseSqlServer(connectionString); });
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", x => x.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
